9bfbe2be4a86f8d994dcbb5b491b1ce874fd8c0a
oncomouse <oncomouse@gmail.com>
2019-06-26 20:40:30 -0500

    Simplified using chain

--- ch08/age.js
+++ ch08/age.js
@@ -14,22 +14,11 @@ var getAge = R.curry((now, user) => R.compose(
   R.map(
     (date) => now.diff(date, 'years')
   ),
-  either(
-    R.identity,
-    R.ifElse(
-      (date) => date.isValid(),
-      Ok,
-      R.always(Err('Unable to Parse Date'))
-    )
-  ),
-  R.map((date) => moment(date, 'YYYY-MM-DD')),
+  R.chain((date) => date.isValid() ? Result.of(date) : Err('Unable to parse date.')),
+  R.chain((date) => Result.of(moment(date, 'YYYY-MM-DD'))),
   safeProp('birthdate')
 )(user));
 
-// var getAge = R.curry(function(now, user) {
-//   var birthdate = moment(user.birthdate, 'YYYY-MM-DD');
-//   return birthdate.isValid() ? Ok(now.diff(birthdate, 'years')) : Err('Birth date could not be parsed');
-// });
 var fortune = R.compose(R.concat('If you survive, you will be '), R.toString, R.add(1));
 var zoltar = R.compose(console.log, either(R.identity, fortune), getAge(moment()));
 
