0775ff6a449b90325c79bc6802cd377f930f5fd2
oncomouse <oncomouse@gmail.com>
2019-06-27 10:12:04 -0500

    Type definitions

--- utils/matchWith.js
+++ utils/matchWith.js
@@ -1,11 +1,17 @@
 const compose = require('crocks/helpers/compose');
 const curry = require('crocks/helpers/curry');
+// head :: [a] -> a
 const head = xs => xs[0];
+// split :: String -> String -> [String]
 const split = curry((s, xs) => xs.split(s));
+// toString :: a -> String
 const toString = m => m.toString();
+// has :: a -> Object -> Boolean
 const has = curry((key, obj) => Object.prototype.hasOwnProperty.call(obj, key));
+// getType :: Union a => a -> String
 const getType = compose(head, split(' '), toString);
 const ANY = 'ANY';
+// matchWith :: Union a => {String: (b -> *)} -> a -> *
 const matchWith = (defs, value) => {
   const type = getType(value);
   if (has(type, defs)) {
